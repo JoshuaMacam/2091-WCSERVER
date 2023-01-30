4fa11ccf9f8741dd8ac0b8c4f8589ec8504b23ba
JoshuaMacam <122767207+JoshuaMacam@users.noreply.github.com>
2023-01-30 13:08:13 +0800

    1 file modified

--- prelims/Prelim-Lab-Activities/Prelim-Lab-Act-3/Lab-Act-3-NodeJS-Server.js
+++ prelims/Prelim-Lab-Activities/Prelim-Lab-Act-3/Lab-Act-3-NodeJS-Server.js
@@ -13,7 +13,7 @@ var server = http.createServer(function (req, res) {
 
     //set responce content
     res.write(
-      '<html><head><h1>Welcome to my Node.js Application</h1></head><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+      '<html><head><h1>Welcome to my Node.js Application</h1></head><body><p>Welcome Joshua Emmanuel Macam. This is an activity about basics of Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/about') {
@@ -22,7 +22,7 @@ var server = http.createServer(function (req, res) {
 
     //set responce content
     res.write(
-      '<html><head><h1>This is the About Page</h1></head><body><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+      '<html><head><h1>This is the About Page</h1></head><body><p>Hello Joshua Emmanuel Macam. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/contact') {
@@ -31,7 +31,7 @@ var server = http.createServer(function (req, res) {
 
     //set responce content
     res.write(
-      '<html><head><h1>This is the Contact Page</h1></head><body><p>John Smith, if you want additional details about this activity go to this site: https://www.tutorialsteacher.com/nodes.js-tutorials</p></body></html>'
+      '<html><head><h1>This is the Contact Page</h1></head><body><p>Joshua Emmanuel Macam, if you want additional details about this activity go to this site: https://www.tutorialsteacher.com/nodes.js-tutorials</p></body></html>'
     );
     res.end();
   } else if (req.url == '/gallery') {
