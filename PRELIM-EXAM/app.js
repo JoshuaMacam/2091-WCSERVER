eea7c0d0ef0ef20f294af34a556e3ae659c4accf
JoshuaMacam <122767207+JoshuaMacam@users.noreply.github.com>
2023-02-08 15:59:54 +0800

    2 files added

---
+++ PRELIM-EXAM/app.js
@@ -0,0 +1,5 @@
+var message = require('./message.js');
+
+message.greet('Joshua');
+message.congrats('Joshua');
+message.farewell('Joshua');
---
+++ PRELIM-EXAM/message.js
@@ -0,0 +1,13 @@
+var message = {
+  greet: function greet(name) {
+    console.log('Hello, ', name);
+  },
+  congrats: function congrats(name) {
+    console.log('Congratulations, ', name);
+  },
+  farewell: function farewell(name) {
+    console.log('Goodbye, ', name);
+  },
+};
+
+module.exports = message;
